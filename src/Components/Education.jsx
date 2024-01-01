c5cfb4c3a273ab70f77c77ad5ee1d511e02b4d0a
Suraj Bhattarai <66477413+ImmrBhattarai@users.noreply.github.com>
2024-01-01 18:40:28 +0000

    Update Education component and Footer text

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 /**
  * Education background image
  */
@@ -7,19 +7,29 @@ import image from "../images/education.jpg";
 
 const Education = () => {
   return (
-    <section className="light" id="education">
+    <section className="min-height" id="education">
       <img className="background" src={image} alt="" />
       <div style={{
         position: "absolute",
         top: "5rem",
         left: "2rem",
         width: "17rem",
-        color: "azure",
+        color: "azure"
         }}>
-        <h2>Engineering Student</h2>
+        <h3>Engineering Student</h3>
       </div>
     </section>
   );
+};
+
+Education.defaultProps = {
+  name: "",
+  title: "",
+};
+
+Education.propTypes = {
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 };
 
 export default Education;
